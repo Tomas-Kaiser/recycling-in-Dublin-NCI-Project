d5337883d760bbd845504088ff0ffea60d33558e
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-12-11 19:55:59 +0000

    Added changes

--- assets/js/leafletMap.js
+++ assets/js/leafletMap.js
@@ -2,77 +2,86 @@
 let venues = [{
    name: "Coolmine Recycling Centre",
    latitude: 53.384570,
-   longtitude: -6.396490
+   longtitude: -6.396490,
+   address: "Coolmine Industrial Estate, Dublin"
  },
  {
-   name: "Coolmine Recycling Centre",
+   name: "Dublin City Council Bring Centre",
    latitude: 53.398567,
-   longtitude: -6.202999
+   longtitude: -6.202999,
+   address: "15 Beechpark Ave, Kilmore, Dublin 5"
  },
  {
    name: "Grangegorman Bring Centre",
    latitude: 53.365801,
-   longtitude: -6.277157
+   longtitude: -6.277157,
+   address: "7 Grangegorman Lower, Arran Quay, Dublin, 7"
  },
  {
    name: "Bottle Bank - Eastwall",
    latitude: 53.351529,
-   longtitude: -6.234484
+   longtitude: -6.234484,
+   address: "E Wall Rd, East Wall, Dublin 3"
  },
  {
    name: "North Strand Recycling Centre",
    latitude: 53.361130,
-   longtitude: -6.242858
+   longtitude: -6.242858,
+   address: "Shamrock Terrace, North Strand, Dublin 1"
  },
  {
    name: "Bottle Bank",
    latitude: 53.350679,
-   longtitude: -6.282341
+   longtitude: -6.282341,
+   address: "Bridgefoot St, The Liberties, Dublin"
  },
  {
    name: "Ringsend Recycling Centre",
    latitude: 53.343835,
-   longtitude: -6.214380
+   longtitude: -6.214380,
+   address: "Pigeon House Rd, Ringsend, Dublin 4"
  },
  {
    name: "Recycling bank",
    latitude: 53.336715,
-   longtitude: -6.225613
- },
- {
-   name: "Interrec Ireland Recycling Centre",
-   latitude: 53.336715,
-   longtitude: -6.225613
+   longtitude: -6.225613,
+   address: "79 Lansdowne Rd, Dublin 4"
  },
  {
    name: "Bring Recycling Centre",
    latitude: 53.327245,
-   longtitude: -6.207020
+   longtitude: -6.207020,
+   address: "sandymount, 143 R131, Dublin Southside, Dublin"
  },
  {
    name: "Bring Recycling Centre",
    latitude: 53.327142,
-   longtitude: -6.264735
+   longtitude: -6.264735,
+   address: "Rathmines, Dublin"
  },
  {
    name: "Bring Recycling Centre",
    latitude: 53.328063,
-   longtitude: -6.308545
+   longtitude: -6.308545,
+   address: "47 Windmill Rd, Crumlin, Dublin 12, D12 C6T8"
  },
  {
    name: "Windmill Road Civic Amenity Centre",
    latitude: 53.328063,
-   longtitude: -6.289147
+   longtitude: -6.289147,
+   address: "244 Clogher Rd, Kimmage, Harold's Cross, Dublin 12"
  },
  {
    name: "Ballymount Civic Amenity Site",
    latitude: 53.312990,
-   longtitude: -6.347168
+   longtitude: -6.347168,
+   address: "Ballymount Ave, Greenhills, Dublin 24"
  },
  {
    name: "Ballyogan Recycling Park",
    latitude: 53.257201,
-   longtitude: -6.189479
+   longtitude: -6.189479,
+   address: "Ballyogan, Dublin"
  }
  
  ];
@@ -92,5 +101,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 venues.map(venue => {
  var marker = L.marker([venue.latitude, venue.longtitude]).addTo(mymap);
 
- marker.bindPopup(`<h5>${venue.name}</h5>`);
+ marker.bindPopup(`
+  <h5>${venue.name}</h5>
+  <address>${venue.address}</address>
+  `);
 });

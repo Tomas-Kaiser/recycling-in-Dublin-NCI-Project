415d52957d21fdf22d46bb65a10e0a9319290dee
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-12-12 11:41:02 +0000

    Added Iffy for scroll up button and leaflet

--- assets/js/leafletMap.js
+++ assets/js/leafletMap.js
@@ -1,108 +1,110 @@
-// Venues where you can recycle
-let venues = [{
-   name: "Coolmine Recycling Centre",
-   latitude: 53.384570,
-   longtitude: -6.396490,
-   address: "Coolmine Industrial Estate, Dublin"
- },
- {
-   name: "Dublin City Council Bring Centre",
-   latitude: 53.398567,
-   longtitude: -6.202999,
-   address: "15 Beechpark Ave, Kilmore, Dublin 5"
- },
- {
-   name: "Grangegorman Bring Centre",
-   latitude: 53.365801,
-   longtitude: -6.277157,
-   address: "7 Grangegorman Lower, Arran Quay, Dublin, 7"
- },
- {
-   name: "Bottle Bank - Eastwall",
-   latitude: 53.351529,
-   longtitude: -6.234484,
-   address: "E Wall Rd, East Wall, Dublin 3"
- },
- {
-   name: "North Strand Recycling Centre",
-   latitude: 53.361130,
-   longtitude: -6.242858,
-   address: "Shamrock Terrace, North Strand, Dublin 1"
- },
- {
-   name: "Bottle Bank",
-   latitude: 53.350679,
-   longtitude: -6.282341,
-   address: "Bridgefoot St, The Liberties, Dublin"
- },
- {
-   name: "Ringsend Recycling Centre",
-   latitude: 53.343835,
-   longtitude: -6.214380,
-   address: "Pigeon House Rd, Ringsend, Dublin 4"
- },
- {
-   name: "Recycling bank",
-   latitude: 53.336715,
-   longtitude: -6.225613,
-   address: "79 Lansdowne Rd, Dublin 4"
- },
- {
-   name: "Bring Recycling Centre",
-   latitude: 53.327245,
-   longtitude: -6.207020,
-   address: "sandymount, 143 R131, Dublin Southside, Dublin"
- },
- {
-   name: "Bring Recycling Centre",
-   latitude: 53.327142,
-   longtitude: -6.264735,
-   address: "Rathmines, Dublin"
- },
- {
-   name: "Bring Recycling Centre",
-   latitude: 53.328063,
-   longtitude: -6.308545,
-   address: "47 Windmill Rd, Crumlin, Dublin 12, D12 C6T8"
- },
- {
-   name: "Windmill Road Civic Amenity Centre",
-   latitude: 53.328063,
-   longtitude: -6.289147,
-   address: "244 Clogher Rd, Kimmage, Harold's Cross, Dublin 12"
- },
- {
-   name: "Ballymount Civic Amenity Site",
-   latitude: 53.312990,
-   longtitude: -6.347168,
-   address: "Ballymount Ave, Greenhills, Dublin 24"
- },
- {
-   name: "Ballyogan Recycling Park",
-   latitude: 53.257201,
-   longtitude: -6.189479,
-   address: "Ballyogan, Dublin"
- }
- 
- ];
+(function () {
+  // Venues where you can recycle
+  let venues = [
+    {
+      name: "Coolmine Recycling Centre",
+      latitude: 53.384570,
+      longtitude: -6.396490,
+      address: "Coolmine Industrial Estate, Dublin"
+    },
+    {
+      name: "Dublin City Council Bring Centre",
+      latitude: 53.398567,
+      longtitude: -6.202999,
+      address: "15 Beechpark Ave, Kilmore, Dublin 5"
+    },
+    {
+      name: "Grangegorman Bring Centre",
+      latitude: 53.365801,
+      longtitude: -6.277157,
+      address: "7 Grangegorman Lower, Arran Quay, Dublin, 7"
+    },
+    {
+      name: "Bottle Bank - Eastwall",
+      latitude: 53.351529,
+      longtitude: -6.234484,
+      address: "E Wall Rd, East Wall, Dublin 3"
+    },
+    {
+      name: "North Strand Recycling Centre",
+      latitude: 53.361130,
+      longtitude: -6.242858,
+      address: "Shamrock Terrace, North Strand, Dublin 1"
+    },
+    {
+      name: "Bottle Bank",
+      latitude: 53.350679,
+      longtitude: -6.282341,
+      address: "Bridgefoot St, The Liberties, Dublin"
+    },
+    {
+      name: "Ringsend Recycling Centre",
+      latitude: 53.343835,
+      longtitude: -6.214380,
+      address: "Pigeon House Rd, Ringsend, Dublin 4"
+    },
+    {
+      name: "Recycling bank",
+      latitude: 53.336715,
+      longtitude: -6.225613,
+      address: "79 Lansdowne Rd, Dublin 4"
+    },
+    {
+      name: "Bring Recycling Centre",
+      latitude: 53.327245,
+      longtitude: -6.207020,
+      address: "sandymount, 143 R131, Dublin Southside, Dublin"
+    },
+    {
+      name: "Bring Recycling Centre",
+      latitude: 53.327142,
+      longtitude: -6.264735,
+      address: "Rathmines, Dublin"
+    },
+    {
+      name: "Bring Recycling Centre",
+      latitude: 53.328063,
+      longtitude: -6.308545,
+      address: "47 Windmill Rd, Crumlin, Dublin 12, D12 C6T8"
+    },
+    {
+      name: "Windmill Road Civic Amenity Centre",
+      latitude: 53.328063,
+      longtitude: -6.289147,
+      address: "244 Clogher Rd, Kimmage, Harold's Cross, Dublin 12"
+    },
+    {
+      name: "Ballymount Civic Amenity Site",
+      latitude: 53.312990,
+      longtitude: -6.347168,
+      address: "Ballymount Ave, Greenhills, Dublin 24"
+    },
+    {
+      name: "Ballyogan Recycling Park",
+      latitude: 53.257201,
+      longtitude: -6.189479,
+      address: "Ballyogan, Dublin"
+    }
+  ];
 
-// Initialize the map
-var mymap = L.map('mapid').setView([53.349804, -6.260310], 11);
+  // Initialize the map
+  var mymap = L.map('mapid').setView([53.349804, -6.260310], 11);
 
-// get layer
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
- attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
- maxZoom: 18,
- id: 'mapbox/streets-v11',
- accessToken: 'pk.eyJ1IjoidG9tYXMta2Fpc2VyIiwiYSI6ImNrM3hrMjh6dzBjdnAzcnM4dDc3em52M3kifQ.DYyTeQdTHi3e0A43TjFBXw'
-}).addTo(mymap);
+  // get layer
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  accessToken: 'pk.eyJ1IjoidG9tYXMta2Fpc2VyIiwiYSI6ImNrM3hrMjh6dzBjdnAzcnM4dDc3em52M3kifQ.DYyTeQdTHi3e0A43TjFBXw'
+  }).addTo(mymap);
 
-// Iterate through the venues array and show the marks on the map
-venues.map(venue => {
- var marker = L.marker([venue.latitude, venue.longtitude]).addTo(mymap);
+  // Iterate through the venues array and show the marks on the map
+  venues.map(venue => {
+  var marker = L.marker([venue.latitude, venue.longtitude]).addTo(mymap);
 
- marker.bindPopup(`
+  marker.bindPopup(`
   <h5>${venue.name}</h5>
   <address>${venue.address}</address>
   `);
-});
+  });
+})();
--- assets/js/scrollUpButton.js
+++ assets/js/scrollUpButton.js
@@ -1,44 +1,44 @@
-// IIFF TODO and FORM with Netlify
+// IFFE
+(function () {
+   // Declare btn class variables
+   const SCROLL_TOP_BTN_CLASSNAME = "scroll-top-btn";
+   const SCROLL_TOP_BTN_CLASSNAME_SHOW = "scroll-top-btn show"
+   const FA_ARROW_UP_CLASS = "fa fa-arrow-circle-up";
 
+   // Create an element button
+   let scrollTopBtn = document.createElement("button");
+   // Create an elemnt i for
+   let iArrowUp = document.createElement("i");
 
-// Declare btn class variables
-const SCROLL_TOP_BTN_CLASSNAME = "scroll-top-btn";
-const SCROLL_TOP_BTN_CLASSNAME_SHOW = "scroll-top-btn show"
-const FA_ARROW_UP_CLASS = "fa fa-arrow-circle-up";
+   // Set the class to the element
+   scrollTopBtn.setAttribute("class", SCROLL_TOP_BTN_CLASSNAME);
+   // Set the class to the i
+   iArrowUp.setAttribute("class", FA_ARROW_UP_CLASS);
 
-// Create an element button
-let scrollTopBtn = document.createElement("button");
-// Create an elemnt i for
-let iArrowUp = document.createElement("i");
+   // Append to the document
+   document.body.appendChild(scrollTopBtn);
+   scrollTopBtn.appendChild(iArrowUp);
 
-// Set the class to the element
-scrollTopBtn.setAttribute("class", SCROLL_TOP_BTN_CLASSNAME);
-// Set the class to the i
-iArrowUp.setAttribute("class", FA_ARROW_UP_CLASS);
+   // Set func for scroll up smoothly
+   const scrollUpSmoothly = (e) => window.scrollTo({ top: 20, behavior: "smooth" });
 
-// Append to the document
-document.body.appendChild(scrollTopBtn);
-scrollTopBtn.appendChild(iArrowUp);
+   // Set event listener for scrolling up
+   scrollTopBtn.addEventListener("click", scrollUpSmoothly);
 
-// Set func for scroll up smoothly
-const scrollUpSmoothly = (e) => window.scrollTo({ top: 20, behavior: "smooth" });
+   // Set to show and hide the button
+   document.addEventListener("scroll", function () {
 
-// Set event listener for scrolling up
-scrollTopBtn.addEventListener("click", scrollUpSmoothly);
+      const hide = () => scrollTopBtn.setAttribute("class", SCROLL_TOP_BTN_CLASSNAME);
+      const show = () => scrollTopBtn.setAttribute("class", SCROLL_TOP_BTN_CLASSNAME_SHOW);
 
-// Set to show and hide the button
-document.addEventListener("scroll", function () {
-
-   const hide = () => scrollTopBtn.setAttribute("class", SCROLL_TOP_BTN_CLASSNAME);
-   const show = () => scrollTopBtn.setAttribute("class", SCROLL_TOP_BTN_CLASSNAME_SHOW);
-
-   if (document.scrollingElement.scrollTop >= 400) {
-      if (scrollTopBtn.className === SCROLL_TOP_BTN_CLASSNAME) {
-         show();
+      if (document.scrollingElement.scrollTop >= 400) {
+         if (scrollTopBtn.className === SCROLL_TOP_BTN_CLASSNAME) {
+            show();
+         }
+      } else if (scrollTopBtn.className === SCROLL_TOP_BTN_CLASSNAME_SHOW) {
+         hide();
       }
-   } else if (scrollTopBtn.className === SCROLL_TOP_BTN_CLASSNAME_SHOW) {
-      hide();
-   }
 
-   console.log(document.scrollingElement.scrollTop);
-});
+      console.log(document.scrollingElement.scrollTop);
+   });
+})();
